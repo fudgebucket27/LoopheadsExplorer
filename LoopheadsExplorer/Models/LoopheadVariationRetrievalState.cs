66d15aed984b500456234ab0ddcb61201fb15f32
John <john.trembath@glencore.com.au>
2022-01-12 11:32:42 +1000

    added loophead variation retrieval state class

---
+++ LoopheadsExplorer/Models/LoopheadVariationRetrievalState.cs
@@ -0,0 +1,16 @@
+﻿namespace LoopheadsExplorer.Models
+{
+    public enum VariationRetrievalState
+    {
+        yetToRetrieve,
+        retrieving,
+        success,
+        error
+    }
+
+    public class LoopheadVariationRetrievalState
+    {
+        public VariationRetrievalState retrievalState { get; set; } = VariationRetrievalState.yetToRetrieve;
+        public string cid { get; set; }
+    }
+}

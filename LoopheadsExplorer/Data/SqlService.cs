627380656750b9cb30f2170338e4d842a3087c34
John <fudgebucket27@gmail.com>
2022-02-12 15:00:19 +1000

    adding kinda working

--- LoopheadsExplorer/Data/SqlService.cs
+++ LoopheadsExplorer/Data/SqlService.cs
@@ -15,9 +15,25 @@ namespace LoopheadsExplorer.Data
                 var parameters = new { LoopheadNumber = _loopheadNumber  };
                 var result = await db
                     .QueryAsync<LoopheadSqlData>
-                    ("select * from Names where loopheadnumber = @LoopheadNumber",
+                    ("select * from LoopheadNames where loopheadnumber = @LoopheadNumber",
                     parameters);
                 return result.ToList();
+            }
+        }
+
+        public async Task AddName(string _clientUUID, string _loopheadName, int _loopheadNumber)
+        {
+            using (IDbConnection db = new SqlConnection(Environment.GetEnvironmentVariable("SQLAZURECONNSTR_DB", EnvironmentVariableTarget.Machine)))
+            {
+                db.Open();
+                var parameters = new
+                {
+                    ClientUUID = _clientUUID,
+                    LoopheadName = _loopheadName,
+                    LoopheadNumber = _loopheadNumber
+                };
+                await db.ExecuteAsync("INSERT INTO LoopheadNames VALUES (@ClientUUID, @LoopheadNumber, @LoopheadName, GETDATE())", parameters);
+                await db.ExecuteAsync("INSERT INTO LoopheadVotes VALUES (@ClientUUID, @LoopheadNumber, @LoopheadName)", parameters);
             }
         }
     }

b49adaf4df5b05752fefa2efe019eed3bfdd50ea
Fudgey <fudgebucket27@gmail.com>
2022-04-07 07:14:37 +1000

    add image thumbnail

--- LoopheadsExplorer/Models/LoopheadIpfsLink.cs
+++ LoopheadsExplorer/Models/LoopheadIpfsLink.cs
@@ -5,6 +5,9 @@
         public string name { get; set; }
         public string cid { get; set; }
 
+        public int randomNumOne { get; set; }
+        public int randomNumTwo { get; set; }
+
         public List<Loophead> loopheads = new List<Loophead>();
     }
 }

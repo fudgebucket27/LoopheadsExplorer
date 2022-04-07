0f389931d73099978b1f0dca8c111b26695caf74
John <john.trembath@glencore.com.au>
2022-04-07 16:31:30 +1000

    set max cache

--- LoopheadsExplorer/Program.cs
+++ LoopheadsExplorer/Program.cs
@@ -27,6 +27,15 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+var cacheMaxAgeOneWeek = (60 * 60 * 24 * 180).ToString();
+app.UseStaticFiles(new StaticFileOptions
+{
+    OnPrepareResponse = ctx =>
+    {
+        ctx.Context.Response.Headers.Append(
+             "Cache-Control", $"public, max-age={cacheMaxAgeOneWeek}");
+    }
+});
 app.UseStaticFiles();
 
 app.UseRouting();
